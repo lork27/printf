e6be14c19e011c1b3b3240dd2ce969aa8e62d3c7
Drew-24 <85302191+Drew-24@users.noreply.github.com>
2021-07-23 16:17:47 -0400

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -3,10 +3,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <limits.h>
+#include <unistd.h>
 /**
  * _printf - function to print
  * @format: parameter
  * Return: 0
  */
 int _printf(const char *format, ...);
+int _putchar(char c);
 #endif /*_HOLBERTON_H_*/
