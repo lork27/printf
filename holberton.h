09a61febfb38d24eda7babd8a9a2c58d5a72336b
Drew-24 <3239@holbertonschool.com>
2021-07-23 19:47:20 +0000

    Holberton Header with prototypes for printf proyect

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdio.h>
+#include <limits.h>
+/**
+ * _printf - function to print
+ * @format: parameter
+ * Return: 0
+ */
+int _printf(const char *format, ...);
+#endif /*_HOLBERTON_H_*/
