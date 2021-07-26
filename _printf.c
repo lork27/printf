89192ad123ed9c0f21de31ecc551f7a6dc1c3ed4
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-26 20:03:01 +0000

    no comment

--- _printf.c
+++ _printf.c
@@ -24,10 +24,8 @@ int _printf(const char *format, ...)
 
 		else
 		{
-			/**
-			 * if (format[i] == '%' && format[i + 1] == '\0')
-			*	return (-1);
-			*/
+			if (format[i] == '%' && format[i + 1] == '\0')
+				return (-1);
 			totalchars += (_putchar(format[i]));
 		}
 	}
