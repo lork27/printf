723288ce89d9f5424734541d12092f3d00efff2a
lork27 <g.lorca.lamadrid@gmail.com>
2021-07-26 22:49:46 +0000

    tried removing a reduntant line

--- _printf.c
+++ _printf.c
@@ -15,8 +15,6 @@ int _printf(const char *format, ...)
 	if (format[0] == '%' && format[1] == '\0')
 		return (-1);
 	va_start(list, format);
-	if (list == NULL)
-		return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] != '\0')
